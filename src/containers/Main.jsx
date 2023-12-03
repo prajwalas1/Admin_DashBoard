0f71d2e646d845d626f2518f8f55fa1831221853
Prajwal A S <prajwalas8999@gmail.com>
2023-12-03 22:04:40 +0530

    first commit

---
+++ src/containers/Main.jsx
@@ -0,0 +1,15 @@
+import UserManagementInterface from "../components/UserManagementInterface";
+import "../index.css";
+
+import "react-toastify/dist/ReactToastify.css";
+
+function Main() {
+  return (
+    <div className="Main">
+      <UserManagementInterface />
+      
+    </div>
+  );
+}
+
+export default Main;
